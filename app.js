5342e49f50e90b1d4747c1ca345df6b0d5337063
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-07-06 08:52:45 -0400

    Final functionality.

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.get("/", (req, res) => res.sendFile(public + "/index.html"));
 //Takes user to restaurants page based on the id. You drop off the id to give the proper page. That id is only used on the client side
 app.get("/restaurants/:id", (req, res)=> res.sendFile(public +"/restaurants.html"))
 
-
+//This is a catch block. The asterisk is for anything. 
+app.get("*", (req, res)=>{
+    res.sendFile(public+"/404.html")
+})
 
 //Tells the command line where the port is listening
 app.listen(port, () => console.log(`API app listeningport ${port}!`));
